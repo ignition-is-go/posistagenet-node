e1cfc1b51704a15c27dae7e60bc26e3336354676
Kyle Bjordahl <kyle@lucid.rocks>
2019-04-02 23:30:09 -0700

    test: send array of trackers

--- src/sendUdp.ts
+++ src/sendUdp.ts
@@ -1,8 +1,10 @@
 import { createSocket, Socket } from 'dgram'
+import { encodeDataPacket } from './encodeDataPacket'
 import { encodeInfoPacket } from './encodeInfoPacket'
 import { psn } from './types'
 
 let counter = 1
+const startTime = Date.now()
 
 const socket = createSocket({ type: 'udp4', reuseAddr: true })
 socket.bind({ port: psn.DEFAULT_PORT })
@@ -30,15 +32,32 @@ const theTrackedThing: psn.Tracker = {
 
 }
 
+const trackers = Array(10).fill(theTrackedThing).map((t, idx) => ({
+	...t,
+	id: idx,
+	name: `Tracker ${idx}`,
+}))
+
+const timestamp = () => (Date.now() - startTime) * 1000
+
 const sendInfo = (soc: Socket) => {
-	const packet = encodeInfoPacket(counter * 1000, counter, theSystem, [theTrackedThing])
+	const packet = encodeInfoPacket(timestamp(), counter, theSystem, trackers)
 	soc.send(packet, 0, packet.length, psn.DEFAULT_PORT, psn.DEFAULT_MULTICAST_ADDRESS, () => {
 		console.log(`Sending infoPacket "${packet}"`)
 	})
 	counter++
 }
 
+const sendData = (soc: Socket) => {
+	const packet = encodeDataPacket(timestamp(), counter, theSystem, trackers)
+	soc.send(packet, 0, packet.length, psn.DEFAULT_PORT, psn.DEFAULT_MULTICAST_ADDRESS, () => {
+		console.log(`Sending dataPacket "${packet}"`)
+	})
+
+}
+
 socket.on('listening', () => {
 	socket.addMembership(psn.DEFAULT_MULTICAST_ADDRESS)
+	setInterval(() => sendData(socket), 1000 / 5)
 	setInterval(() => sendInfo(socket), 1000)
 })
