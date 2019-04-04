167c72e5d91e2e36ae6e8d29dc0a560707ef944d
Kyle Bjordahl <kyle@lucid.rocks>
2019-04-04 12:42:51 -0700

    fix(index): exports for library

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,14 @@
+import { encodeDataPacket } from './encodeDataPacket'
+import { encodeInfoPacket } from './encodeInfoPacket'
+import { psn as psn_lib } from './types'
+import { multicastHelper as createMulticastSocket } from './utils/multicastHelper'
+
+export const psn = {
+	...psn_lib,
+	encodeDataPacket,
+	encodeInfoPacket,
+	createMulticastSocket,
+}
+
 // require('./sendUdp')
-require('./benchmark')
+// require('./benchmark')
